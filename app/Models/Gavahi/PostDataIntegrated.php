6f4d8da833320fc955ccc309ff279186d2d62b65
mahbobe.9495@gmail.com <mahbobe.9495@gmail.com>
2021-06-19 13:17:08 +0430

    change files by seggestions

---
+++ app/Models/Gavahi/PostDataIntegrated.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PostDataIntegrated extends Model
+{
+    protected $connection = 'pgsql2';
+    protected $table = 'post_data_integrated';
+    public static function getSummery($postalcode)
+    {
+        $item = self::where('postalcode',$postalcode)->get([
+            'postalcode',
+            'pelak',
+            '','','']);
+    }
+}
