f3c0f5c32204556c1e16d6bddd467463e13a7b22
Hamed Zarei <hamedzarei.7232@gmail.com>
2021-06-29 10:06:31 +0430

    change route

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->post('/pdf/{identifier}', 'PdfMakerController@getPdf');
+$router->post('/{identifier}', 'PdfMakerController@getPdf');
 
 
 $router->group(['prefix' => 'interpreters'], function () use ($router) {
