5be0fa2f0256fa39c7aa2469b3a0f60dad020cd1
mahbobe.9495@gmail.com <mahbobe.9495@gmail.com>
2021-07-25 11:16:47 +0430

    database bog fixed

--- app/Http/Services/PdfMakerService.php
+++ app/Http/Services/PdfMakerService.php
@@ -51,19 +51,21 @@ class PdfMakerService
             // $d['parts'] = Part::index(Tour::getId($data['tour_no']));
             $d['parts'] = Part::index($data['tour_id']);
             $parts_count = count($d['parts']);
+
             for ($i = 0; $i < $parts_count; $i++) {
 
                 $d['parts'][$i]['blocks'] = Block::index($d['parts'][$i]['id']);
                 $blocks_count = count($d['parts'][$i]['blocks']);
                 $blocks_c += $blocks_count;
 
+
                 for ($j = 0; $j < $blocks_count; $j++) {
 
                     $d['parts'][$i]['blocks'][$j]['buildings'] =
                         Building::index($d['parts'][$i]['blocks'][$j]['id']);
+
                     $buildings_count = count($d['parts'][$i]['blocks'][$j]['buildings']);
                     $all_buildings += $buildings_count;
-
                     for ($k = 0; $k < $buildings_count; $k++) {
 
                         $d['parts'][$i]['blocks'][$j]['buildings'][$k]['addresses'] =
@@ -77,14 +79,13 @@ class PdfMakerService
                         for ($l = 0; $l < $addresses_count; $l++) {
                             $d['parts'][$i]['blocks'][$j]['buildings'][$k]['addresses'][$l]['entrances'] =
                                 Entrance::index($d['parts'][$i]['blocks'][$j]['buildings'][$k]['addresses'][$l]['id']);
-
                             $s_way_id = $d['parts'][$i]['blocks'][$j]['buildings'][$k]['addresses'][$l]['secondary_way_id'];
                             $way_id = $d['parts'][$i]['blocks'][$j]['buildings'][$k]['addresses'][$l]['way_id'];
 
                             $d['parts'][$i]['blocks'][$j]['buildings'][$k]['addresses'][$l]['way_name'] = Way::getName($way_id);
                             $d['parts'][$i]['blocks'][$j]['buildings'][$k]['addresses'][$l]['s_way_name'] = Way::getName($s_way_id);
-
                             $entrances_count = count($d['parts'][$i]['blocks'][$j]['buildings'][$k]['addresses'][$l]['entrances']);
+
 
                             for ($m = 0; $m < $entrances_count; $m++) {
                                 $d['parts'][$i]['blocks'][$j]['buildings'][$k]['addresses'][$l]['entrances'][$m]['units'] =
@@ -99,13 +100,13 @@ class PdfMakerService
                 }
 
             }
-            $tour_no = Tour::getNo($data['tour_id']);
+            $tour_name = Tour::getName($data['tour_id']);
             if ($identifier == 'notebook_1') {
                 $province_name = Province::getName(Tour::getProvinceId($data['tour_id']));
 
                 //req body tourno,
                 $params = [
-                    "tour_no" => $tour_no,
+                    "tour_no" => $tour_name,
                     "code_joze" => 5,
                     "province" => $province_name,
                     "region" => 'منطقه۹',
@@ -122,7 +123,7 @@ class PdfMakerService
                 ];
             } elseif ($identifier == 'notebook_2') {
                 $params = [
-                    "tour_no" => $tour_no,
+                    "tour_no" => $tour_name,
                     "code_joze" => 5,
                     "page" => 1,
                     "date" => $date,
@@ -132,7 +133,7 @@ class PdfMakerService
 
             } elseif ($identifier == 'notebook_3') {
                 $params = [
-                    "tour_no" => $tour_no,
+                    "tour_no" => $tour_name,
                     "date" => $date,
                     "code_joze" => 5,
                     "way_type" => 'خیابان',
