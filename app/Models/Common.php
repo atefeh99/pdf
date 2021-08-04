6b887acfa86e5e82584b79b1c12de2f917390eee
mahbobe.9495@gmail.com <mahbobe.9495@gmail.com>
2021-08-04 17:37:39 +0430

    qrcode added and files migration added and geo param become optional

---
+++ app/Models/Common.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Models;
+
+use App\Helpers\Date;
+
+trait Common
+{
+
+    public function getCreatedAtAttribute($value)
+    {
+        return Date::convertCarbonToJalali($value);
+    }
+    public function getUpdatedAtAttribute($value)
+    {
+        return Date::convertCarbonToJalali($value);
+    }
+    public function getBarcodesAttribute($value)
+    {
+        return json_decode($value, true);
+    }
+    public function setBarcodesAttribute($value)
+    {
+        $this->attributes['barcodes'] = json_encode($value);
+    }
+
+}
