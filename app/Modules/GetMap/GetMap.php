9c7af267010830582eb7ba8555ecf3e5550ed22a
mahbobe.9495@gmail.com <mahbobe.9495@gmail.com>
2021-11-13 17:34:32 +0330

    edit pdf

--- app/Modules/GetMap/GetMap.php
+++ app/Modules/GetMap/GetMap.php
@@ -19,7 +19,7 @@ class GetMap
 
         curl_setopt_array($curl, array(
 //            CURLOPT_URL => 'localhost:8080?width=1400&height=800&markers=color:red%7Clabel:a%7C51.394912,35.72164&zoom_level=14&type=vector&style=light',
-            CURLOPT_URL => 'localhost:8080?width=1400&height=800&markers=color:red%7Clabel:a%7C51.394912,35.72164&zoom_level=14&type=vector&style=light',
+            CURLOPT_URL => 'localhost:8080?width=1400&height=800&markers=color:red%7Clabel:a%7C51.394912,35.72164&zoom_level=10&type=vector&style=light',
 
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
