7098a0b9682b7eb474ff17ff73581b50d0dcd7a5
mahbobe.9495@gmail.com <mahbobe.9495@gmail.com>
2021-12-07 14:59:22 +0330

    edit direct_mail

--- app/Http/Controllers/RulesTrait.php
+++ app/Http/Controllers/RulesTrait.php
@@ -112,7 +112,7 @@ trait RulesTrait
 
         }
         if ($validation->fails()) {
-            dd($validation->errors()->getMessages());
+//            dd($validation->errors()->getMessages());
             throw new RequestRulesException($validation->errors()->getMessages(), $code);
         }
 
