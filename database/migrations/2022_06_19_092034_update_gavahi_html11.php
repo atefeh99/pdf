92c04357a2a4a4795bba4af87fd4e45d2d63254f
Hamed Zarei <hamedzarei.7232@gmail.com>
2022-06-19 18:35:31 +0430

    debug migration

--- database/migrations/2022_06_19_092034_update_gavahi_html11.php
+++ database/migrations/2022_06_19_092034_update_gavahi_html11.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateGavahiHtml9 extends Migration
+class UpdateGavahiHtml11 extends Migration
 {
     /**
      * Run the migrations.
